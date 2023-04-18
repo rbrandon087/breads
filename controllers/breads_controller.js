4503dc68a4c1beac6c6ff39ae077e322d852e0d4
Brandon <rbrandon087@gmail.com>
2023-04-17 20:58:13 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,9 +2,15 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
 })
 
 
