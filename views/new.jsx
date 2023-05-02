0558170c1315b6a2efb1f5bb4043a8289f2998ac
Brandon <rbrandon087@gmail.com>
2023-05-01 19:19:46 -0700

    fixed my code in my new.js function

--- views/new.jsx
+++ views/new.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function New (bakers) {
+function New ({bakers}) {
     return (
       <Default>
         <h2>Add a new bread</h2>
